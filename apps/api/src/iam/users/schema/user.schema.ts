912d80d5af6934c0a4a6fe9f01923b8d9383999a
Mark Njoroge <marknjo7@gmail.com>
2023-02-02 13:38:45 +0300

    refactor(apps/UserSchema): add totalTeamMembers field

--- apps/api/src/iam/users/schema/user.schema.ts
+++ apps/api/src/iam/users/schema/user.schema.ts
@@ -64,6 +64,11 @@ export class User {
 
   @Prop()
   passwordResetExpiresAt?: Date
+
+  @Prop({
+    default: 0,
+  })
+  totalTeamMembers?: number
 }
 
 export const UserSchema = SchemaFactory.createForClass(User)
