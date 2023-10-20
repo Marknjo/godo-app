caa8ad595196e5086cfc6affc9e794d5160c06c9
Mark Njoroge <marknjo7@gmail.com>
2023-10-20 14:14:38 +0300

    chore(api/App): add test handlers & services method

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,17 +1,37 @@
-import { Controller, Get } from '@nestjs/common'
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common'
 import { AppService } from './app.service'
 import { Serialize } from './common/decorators/serialize.decorator'
 import { AppResponseDto } from './dtos/app-response.dto'
+import { MongoIdPipe } from './common/pipes/mongo-id.pipe'
+import { CreateAppDto } from './dtos/create-app.dto'
+import { AppQueryDto } from './dtos/app-query.dto'
 
+@Serialize(AppResponseDto)
 @Controller({
   version: '1',
 })
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Serialize(AppResponseDto)
   @Get()
-  getHello() {
-    return this.appService.getHello()
+  getHello(@Query() query: AppQueryDto) {
+    return this.appService.getHello(query)
+  }
+
+  @Post()
+  getHelloPost(@Body() content: CreateAppDto) {
+    return this.appService.getHolloPost(content)
+  }
+
+  /**
+   *
+   * @param id
+   * @returns
+   */
+  @Get(':id')
+  findOneHello(@Param('id', MongoIdPipe) id: string) {
+    return {
+      value: `${id}`,
+    }
   }
 }
--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common'
+import { CreateAppDto } from './dtos/create-app.dto'
+import { AppQueryDto } from './dtos/app-query.dto'
 
 @Injectable()
 export class AppService {
-  getHello() {
+  getHello(query: AppQueryDto) {
     return {
       data: {
-        value: 'Hello World!',
+        value: `Hello ${query?.name || 'World'}!`,
       },
       page: 3,
       limit: 10,
       totalResults: 400,
+    }
+  }
+
+  getHolloPost(content: CreateAppDto) {
+    return {
+      data: content,
     }
   }
 }
