38083bf8255e7e8e96ffe80ea07e9e34821e7f02
Mark Njoroge <marknjo7@gmail.com>
2023-10-21 15:13:31 +0300

    feat(api/Users): define CreateUserDto class

--- apps/api/src/iam/users/dto/create-user.dto.ts
+++ apps/api/src/iam/users/dto/create-user.dto.ts
@@ -1,1 +1,46 @@
-export class CreateUserDto {}
+import { Transform } from 'class-transformer'
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator'
+import { Match } from 'src/common/validators/match.validator'
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(50, { message: 'Username must not exceed 50 characters' })
+  @Transform(({ value }) => value.trim())
+  username: string
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(100, { message: 'Email must not exceed 100 characters' })
+  @Transform(({ value }) => value.trim())
+  email: string
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(255, { message: 'Password must not exceed 255 characters' })
+  @MinLength(6, { message: 'Password should be greater than 6 character' })
+  @Transform(({ value }) => value.trim())
+  password: string
+
+  @IsNotEmpty()
+  @IsString()
+  @Match('password', { message: 'Password do not match' })
+  passwordConfirm: string
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(1000, { message: 'Bio must not exceed 1000 characters' })
+  @Transform(({ value }) => value.trim())
+  bio?: string
+
+  @IsOptional()
+  @IsString()
+  @Transform(({ value }) => value.trim())
+  profileImg?: string
+}
