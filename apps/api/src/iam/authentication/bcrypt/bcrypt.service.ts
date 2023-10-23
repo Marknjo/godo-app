f394b8969f0c1d27a78aefa0b0418b92351ee62a
Mark Njoroge <marknjo7@gmail.com>
2023-10-23 14:14:55 +0300

    feat(api/Iam): generate BcryptService & implement it's HashService methods, compare & hash

---
+++ apps/api/src/iam/authentication/bcrypt/bcrypt.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common'
+import { HashService } from './hash.service'
+import { compare, genSalt, hash } from 'bcrypt'
+
+@Injectable()
+export class BcryptService implements HashService {
+  async hash(data: string): Promise<string> {
+    const salt = await genSalt(11)
+
+    return hash(data, salt)
+  }
+  async compare(plainData: string, hashedData: string): Promise<boolean> {
+    return compare(plainData, hashedData)
+  }
+}
