76038ed51269caeef54404efba0bcbff75e7b060
Mark Njoroge <marknjo7@gmail.com>
2023-10-26 14:40:22 +0300

    feat(api/Iam): implement RolesResponseDto

---
+++ apps/api/src/iam/authorization/roles/dto/role-response.dto.ts
@@ -0,0 +1,42 @@
+import { Expose, Type } from 'class-transformer'
+import { ERoleTypes } from '../enums/e-role-types'
+
+export class SlimRoleResponseDto {
+  @Expose()
+  id: string
+
+  @Expose()
+  name: string
+
+  @Expose()
+  isEnabled: boolean
+}
+
+export class RoleResponseDto {
+  @Expose()
+  id: string
+
+  @Expose()
+  name: string
+
+  @Expose()
+  description: string
+
+  @Expose()
+  isEnabled: boolean
+
+  @Expose()
+  @Type(() => SlimRoleResponseDto)
+  assignedFor: SlimRoleResponseDto
+
+  @Expose()
+  type: ERoleTypes
+
+  @Expose()
+  @Type(() => Date)
+  createdAt: Date
+
+  @Expose()
+  @Type(() => Date)
+  updatedAt: Date
+}
