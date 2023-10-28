4b410a379da7bc15283bd0e3533b6528f44b3973
Mark Njoroge <marknjo7@gmail.com>
2023-10-28 13:44:09 +0300

    feat(api/Accesses): define CreateAccessDto

--- apps/api/src/iam/authorization/accesses/dto/create-access.dto.ts
+++ apps/api/src/iam/authorization/accesses/dto/create-access.dto.ts
@@ -1,1 +1,11 @@
-export class CreateAccessDto {}
+import { User } from 'src/iam/users/schema/user.schema'
+import { Role } from '../../roles/schema/role.schema'
+import { IsMongoId } from 'class-validator'
+
+export class CreateAccessDto {
+  @IsMongoId({ message: 'Invalid user' })
+  assignedTo: User
+
+  @IsMongoId({ message: 'Invalid role' })
+  roleId: Role
+}
