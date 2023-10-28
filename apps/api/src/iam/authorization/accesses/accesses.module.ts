d455e916db14d1f078add198039e741b9c45f1c0
Mark Njoroge <marknjo7@gmail.com>
2023-10-28 13:45:29 +0300

    ci(api/AccessesModule): setup default imports & providers including Access scheme

--- apps/api/src/iam/authorization/accesses/accesses.module.ts
+++ apps/api/src/iam/authorization/accesses/accesses.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+
+import { FactoryUtils } from 'src/common/services/factory.utils'
+import { UsersModule } from 'src/iam/users/users.module'
+
 import { AccessesService } from './accesses.service'
 import { AccessesController } from './accesses.controller'
+import { RolesModule } from '../roles/roles.module'
+import { Access, AccessSchema } from './schema/access.schema'
 
 @Module({
+  imports: [
+    RolesModule,
+    UsersModule,
+    MongooseModule.forFeatureAsync([
+      {
+        name: Access.name,
+        useFactory: () => {
+          const schema = AccessSchema
+
+          return schema
+        },
+      },
+    ]),
+  ],
   controllers: [AccessesController],
-  providers: [AccessesService],
+  providers: [AccessesService, FactoryUtils],
 })
 export class AccessesModule {}
