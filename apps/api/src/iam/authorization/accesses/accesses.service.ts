fa2d9227be420814d3f0476795cd250e9b221d39
Mark Njoroge <marknjo7@gmail.com>
2023-10-28 13:47:22 +0300

    ci(api/AccessesService): streamline default injects & method parameters across all CRUD method

--- apps/api/src/iam/authorization/accesses/accesses.service.ts
+++ apps/api/src/iam/authorization/accesses/accesses.service.ts
@@ -1,26 +1,52 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger, LoggerService } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { FilterQuery, Model } from 'mongoose'
+
+import { FactoryUtils } from 'src/common/services/factory.utils'
+import { UsersService } from 'src/iam/users/users.service'
+import { IActiveUser } from 'src/iam/interfaces/i-active-user'
+
 import { CreateAccessDto } from './dto/create-access.dto'
 import { UpdateAccessDto } from './dto/update-access.dto'
+import { Access } from './schema/access.schema'
+import { RolesService } from '../roles/roles.service'
 
 @Injectable()
 export class AccessesService {
-  create(createAccessDto: CreateAccessDto) {
+  private readonly logger: LoggerService = new Logger(AccessesService.name)
+
+  constructor(
+    @InjectModel(Access.name)
+    private readonly accessesModel: Model<Access>,
+
+    private readonly usersService: UsersService,
+
+    private readonly rolesService: RolesService,
+
+    private readonly factoryUtils: FactoryUtils,
+  ) {}
+
+  create(createAccessDto: CreateAccessDto, activeUser: IActiveUser) {
     return 'This action adds a new access'
   }
 
-  findAll() {
-    return `This action returns all accesses`
+  findAll(activeUser: IActiveUser, filters?: FilterQuery<Access>) {
+    return `This action returns all accesses ${activeUser}`
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} access`
+  findOne(accessId: string, activeUser: IActiveUser) {
+    return `This action returns a #${accessId} access`
   }
 
-  update(id: number, updateAccessDto: UpdateAccessDto) {
-    return `This action updates a #${id} access`
+  update(
+    accessId: string,
+    updateAccessDto: UpdateAccessDto,
+    activeUser: IActiveUser,
+  ) {
+    return `This action updates a #${accessId} access`
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} access`
+  remove(accessId: string, activeUser: IActiveUser) {
+    return `This action removes a #${accessId} access`
   }
 }
