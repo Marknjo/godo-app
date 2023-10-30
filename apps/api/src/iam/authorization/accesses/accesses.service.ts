bca385653defb71cba0449acbb7d20c03aa24b21
Mark Njoroge <marknjo7@gmail.com>
2023-10-30 14:24:29 +0300

    feat(api/AccessesService): implement validateRequestAction() method to ensure users have rights to add/update/delete an access

--- apps/api/src/iam/authorization/accesses/accesses.service.ts
+++ apps/api/src/iam/authorization/accesses/accesses.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, Logger, LoggerService } from '@nestjs/common'
+import {
+  ForbiddenException,
+  Injectable,
+  Logger,
+  LoggerService,
+} from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { FilterQuery, Model } from 'mongoose'
 
@@ -10,6 +15,8 @@ import { CreateAccessDto } from './dto/create-access.dto'
 import { UpdateAccessDto } from './dto/update-access.dto'
 import { Access } from './schema/access.schema'
 import { RolesService } from '../roles/roles.service'
+import { EPremiumRoles, ERoles } from 'src/iam/enums/e-roles.enum'
+import { ERoleTypes } from '../roles/enums/e-role-types'
 
 @Injectable()
 export class AccessesService {
@@ -26,19 +33,26 @@ export class AccessesService {
     private readonly factoryUtils: FactoryUtils,
   ) {}
 
-  create(createAccessDto: CreateAccessDto, activeUser: IActiveUser) {
-    return 'This action adds a new access'
+  async create(createAccessDto: CreateAccessDto, activeUser: IActiveUser) {
+    // ensure user can create an access before proceeding
+    const errorMessage =
+      'Failed to assign the requested role because you lack enough credentials'
+    const roleToAssign = createAccessDto.roleId.toString()
+
+    await this.validateRequestAction(activeUser, roleToAssign, errorMessage)
+
+    // create new access
   }
 
-  findAll(activeUser: IActiveUser, filters?: FilterQuery<Access>) {
+  async findAll(activeUser: IActiveUser, filters?: FilterQuery<Access>) {
     return `This action returns all accesses ${activeUser}`
   }
 
-  findOne(accessId: string, activeUser: IActiveUser) {
+  async findOne(accessId: string, activeUser: IActiveUser) {
     return `This action returns a #${accessId} access`
   }
 
-  update(
+  async update(
     accessId: string,
     updateAccessDto: UpdateAccessDto,
     activeUser: IActiveUser,
@@ -46,7 +60,67 @@ export class AccessesService {
     return `This action updates a #${accessId} access`
   }
 
-  remove(accessId: string, activeUser: IActiveUser) {
+  async remove(accessId: string, activeUser: IActiveUser) {
     return `This action removes a #${accessId} access`
+  }
+
+  /**
+   * --------------------------------------------
+   *                     HELPERS
+   * --------------------------------------------
+   */
+
+  /**
+   * Ensures that a user trying to create a role has the right to create it
+   * @param activeUser
+   * @param createAccessDto
+   */
+  private async validateRequestAction(
+    activeUser: IActiveUser,
+    roleId: string,
+    errorMessage: string,
+  ) {
+    const { sub: accountOwner, baseRole, role } = activeUser
+
+    const whoIs = this.factoryUtils.whoIs(activeUser)
+    const managerId = whoIs === accountOwner ? undefined : whoIs
+
+    // find type of a the current  user belongs
+    const type =
+      role === ERoles.ADMIN || baseRole === EPremiumRoles.ADMIN
+        ? ERoleTypes.ADMIN
+        : ERoleTypes.REGULAR
+
+    // fetch the role user want to assign
+    const roleData = await this.rolesService.findOne(roleId, activeUser, type, {
+      isEnabled: true,
+    })
+    const roleName = roleData.name
+
+    // validations
+    if (managerId) {
+      // handling premium, guest & teams manager
+      if (baseRole !== EPremiumRoles.ADMIN && roleName !== ERoles.MEMBER) {
+        throw new ForbiddenException(errorMessage)
+      }
+
+      // admin manager
+      if (
+        baseRole === EPremiumRoles.ADMIN &&
+        (roleName === ERoles.ADMIN ||
+          roleName === ERoles.MANAGER ||
+          roleName === ERoles.MEMBER ||
+          roleName === ERoles.ADMIN_MANAGER)
+      ) {
+        throw new ForbiddenException(errorMessage)
+      }
+    }
+
+    // handle account owners
+    if (role !== ERoles.ADMIN) {
+      if (roleName !== ERoles.MANAGER && roleName !== ERoles.MEMBER) {
+        throw new ForbiddenException(errorMessage)
+      }
+    }
   }
 }
