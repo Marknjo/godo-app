6ca717d7ddabdd9a76403667422a9fdcf3e5f70c
Mark Njoroge <marknjo7@gmail.com>
2023-10-31 13:39:18 +0300

    feat(api/Teams): define a teams mongodb schema

---
+++ apps/api/src/iam/users/schema/team.schema.ts
@@ -0,0 +1,43 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { User } from './user.schema'
+import { HydratedDocument, SchemaTypes } from 'mongoose'
+
+@Schema({
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+  timestamps: true,
+})
+export class Team {
+  @Prop({
+    type: SchemaTypes.ObjectId,
+    ref: 'User',
+    index: true,
+  })
+  accountOwner: User
+
+  @Prop({
+    type: SchemaTypes.ObjectId,
+    ref: 'User',
+    index: true,
+  })
+  memberId: User
+
+  @Prop()
+  description: string
+
+  @Prop({
+    default: true,
+  })
+  isActive?: boolean
+
+  @Prop({
+    default: false,
+  })
+  isResigned?: boolean
+
+  @Prop()
+  resignationReason?: string
+}
+
+export const TeamSchema = SchemaFactory.createForClass(Team)
+export type TTeamDoc = HydratedDocument<Team>
