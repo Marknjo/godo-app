4d5433a0b8a82a22be0b87a147a082b815861992
Mark Njoroge <marknjo7@gmail.com>
2023-11-01 13:58:39 +0300

    ci(apps/UsersModule): import teams model

--- apps/api/src/iam/users/users.module.ts
+++ apps/api/src/iam/users/users.module.ts
@@ -8,6 +8,9 @@ import { UsersController } from './users.controller'
 import { User, UserSchema } from './schema/user.schema'
 import { HashingService } from '../authentication/bcrypt/hashing.service'
 import { BcryptService } from '../authentication/bcrypt/bcrypt.service'
+import { TeamsController } from './teams/teams.controller'
+import { TeamsService } from './teams/teams.service'
+import { Team, TeamSchema } from './schema/team.schema'
 
 @Module({
   imports: [
@@ -16,20 +19,30 @@ import { BcryptService } from '../authentication/bcrypt/bcrypt.service'
         name: User.name,
         useFactory: () => {
           const schema = UserSchema
+
+          return schema
+        },
+      },
+      {
+        name: Team.name,
+        useFactory: (userService: UsersService) => {
+          const schema = TeamSchema
+
           return schema
         },
       },
     ]),
   ],
-  controllers: [UsersController],
+  controllers: [UsersController, TeamsController],
   providers: [
     {
       provide: HashingService,
       useClass: BcryptService,
     },
     UsersService,
+    TeamsService,
     FactoryUtils,
   ],
-  exports: [UsersService],
+  exports: [UsersService, TeamsService],
 })
 export class UsersModule {}
