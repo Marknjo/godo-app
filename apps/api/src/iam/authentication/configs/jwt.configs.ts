81a7cf73e1408af83252a82899ffe6fe99eca648
Mark Njoroge <marknjo7@gmail.com>
2023-11-02 13:11:50 +0300

    feat(api/Iam): add jwtConfigs fields

---
+++ apps/api/src/iam/authentication/configs/jwt.configs.ts
@@ -0,0 +1,12 @@
+import { env } from 'node:process'
+import { registerAs } from '@nestjs/config'
+
+export const jwtConfigs = registerAs('jwt', () => {
+  return {
+    secret: env.JWT_SECRET,
+    audience: env.JWT_TOKEN_AUDIENCE,
+    issuer: env.JWT_TOKEN_ISSUER,
+    accessTokenTTL: env.JWT_ACCESS_TOKEN_TTL,
+    refreshTokenTTL: env.JWT_REFRESH_TOKEN_TTL,
+  }
+})
