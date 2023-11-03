92f03189ea35d50fa3381b5066361947356c0d5f
Mark Njoroge <marknjo7@gmail.com>
2023-11-03 13:27:52 +0300

    refactor(api/RolesService): populate all response

--- apps/api/src/iam/authorization/roles/roles.service.ts
+++ apps/api/src/iam/authorization/roles/roles.service.ts
@@ -1,6 +1,7 @@
 import {
   BadRequestException,
   ConflictException,
+  ForbiddenException,
   Injectable,
   InternalServerErrorException,
   Logger,
@@ -10,10 +11,10 @@ import { CreateRoleDto } from './dto/create-role.dto'
 import { UpdateRoleDto } from './dto/update-role.dto'
 import { FilterQuery, Model } from 'mongoose'
 
-import { Role } from './schema/role.schema'
+import { Role, TRolesDoc } from './schema/role.schema'
 import { InjectModel } from '@nestjs/mongoose'
 import { IActiveUser } from 'src/iam/interfaces/i-active-user'
-import { ERoleTypes } from './enums/e-role-types'
+import { ERoleTypes, ERolesAdmin, ERolesRegular } from './enums/e-role-types'
 import { FactoryUtils } from 'src/common/services/factory.utils'
 import { ToggleRoleDto } from './dto/toggle-role.dto'
 import { ERoles } from 'src/iam/enums/e-roles.enum'
@@ -38,6 +39,8 @@ export class RolesService {
     this.logger.log(`User with id: ${whoIs} is creating a new role`)
 
     try {
+      this.throwIfRolesMismatch(type, createRoleDto)
+
       // data preparation
       const roleDetails = new Role()
       roleDetails.assignedFor = createRoleDto.assignedFor
@@ -45,10 +48,12 @@ export class RolesService {
       roleDetails.name = createRoleDto?.name
 
       // create new role
-      const newRole = await this.rolesModel.create({
+      let newRole = await this.rolesModel.create({
         ...roleDetails,
         type,
       })
+
+      newRole = await this.populateHelper(newRole)
 
       return newRole
     } catch (error) {
@@ -73,7 +78,7 @@ export class RolesService {
     }
   }
 
-  findAll(
+  async findAll(
     filters: FilterQuery<Role>,
     activeUser: IActiveUser,
     type: ERoleTypes,
@@ -81,10 +86,19 @@ export class RolesService {
     const whoIs = this.factoryUtils.whoIs(activeUser)
     this.logger.log(`User with id: ${whoIs} is accessing all roles`)
 
-    return this.rolesModel.find({
-      ...filters,
-      $and: [{ type }],
-    })
+    const roles = await this.rolesModel
+      .find({
+        ...filters,
+        $and: [{ type }],
+      })
+      .populate<{ assignedFor: TRolesDoc }>({
+        path: 'assignedFor',
+        select: 'id name',
+      })
+
+    return {
+      data: roles,
+    }
   }
 
   async findOne(
@@ -128,6 +142,8 @@ export class RolesService {
 
     // handle update errors
     this.throwIfRoleNotFound(updatedRole, roleId, 'updating')
+
+    await this.populateHelper(updatedRole)
 
     // response
     this.logger.log(`Role with id ${roleId} was successfully updated`)
@@ -179,7 +195,7 @@ export class RolesService {
     filters: FilterQuery<Role> = {},
     type?: ERoleTypes,
   ) {
-    const isERole = !!ERoles[searchBy]
+    const isERole = !!ERoles[searchBy.toUpperCase()]
 
     const foundRole = await this.rolesModel.findOne({
       ...(isERole ? { name: searchBy } : { _id: searchBy, $and: [{ type }] }),
@@ -188,7 +204,19 @@ export class RolesService {
 
     // validation
     this.throwIfRoleNotFound(foundRole, searchBy, 'finding')
+
+    // populate fields
+    await this.populateHelper(foundRole)
+
     return foundRole
+  }
+
+  private async populateHelper(roleDoc: TRolesDoc) {
+    roleDoc = await roleDoc.populate<{ assignedFor: TRolesDoc }>({
+      path: 'assignedFor',
+      select: 'id name',
+    })
+    return roleDoc
   }
 
   private throwIfRoleNotFound(role: Role, roleId: string, action: string) {
@@ -198,6 +226,15 @@ export class RolesService {
       throw new BadRequestException(
         `Oops! looks like  ${action} role with id ${roleId} failed`,
       )
+    }
+  }
+
+  private throwIfRolesMismatch(type: ERoleTypes, createRoleDto: CreateRoleDto) {
+    if (
+      (type === ERoleTypes.ADMIN && !ERolesAdmin[createRoleDto.name]) ||
+      (type === ERoleTypes.REGULAR && !ERolesRegular[createRoleDto.name])
+    ) {
+      throw new ForbiddenException(`${createRoleDto.name} is not type ${type}`)
     }
   }
 }
