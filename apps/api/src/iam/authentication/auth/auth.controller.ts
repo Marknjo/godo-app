2a76d32b31846da4c91d7a51a5998ed42fd97a6d
Mark Njoroge <marknjo7@gmail.com>
2023-11-04 14:44:31 +0300

    feat(api/AuthController): implement /swtch-account route handler (switchAccount() method)

--- apps/api/src/iam/authentication/auth/auth.controller.ts
+++ apps/api/src/iam/authentication/auth/auth.controller.ts
@@ -1,10 +1,14 @@
-import { Body, Controller, Post, Req } from '@nestjs/common'
+import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common'
 import { SignUpDto } from './dtos/sign-up.dto'
 import { SignInDto } from './dtos/sign-in.dto'
 import { AuthService } from './auth.service'
 import { Serialize } from 'src/common/decorators/serialize.decorator'
 import { AuthResponseDto } from './dtos/auth-response.dto'
 import { Request } from 'express'
+import { PerseMongoIdPipe } from 'src/common/pipes/perse-mongo-id.pipe'
+import { ActiveUser } from '../decorators/active-user.decorator'
+import { IActiveUser } from 'src/iam/interfaces/i-active-user'
+import { SwitchedAccountDto } from './dtos/switched-account.dto'
 
 @Serialize(AuthResponseDto)
 @Controller({
@@ -19,8 +23,18 @@ export class AuthController {
     return this.authService.signUp(signUpDto)
   }
 
-  @Post('signin/:dynamic/:more')
+  @Post('signin')
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto)
+  }
+
+  // TODO: Implement authGuard
+  @Serialize(SwitchedAccountDto)
+  @Get('switch-account/:account-owner-id')
+  switchAccount(
+    @Param('account-owner-id', PerseMongoIdPipe) accountOwnerId: string,
+    @ActiveUser() activeUser: IActiveUser,
+  ) {
+    return this.authService.switchAccount(accountOwnerId, activeUser)
   }
 }
