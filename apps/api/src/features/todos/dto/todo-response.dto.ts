7acccd928e8ccbfdadb891b26604f74beb7d1f83
Mark Njoroge <marknjo7@gmail.com>
2023-11-08 12:20:33 +0300

    feat(api/todos): define TodoResponseDto & SlimTodoResponseDto serializer

---
+++ apps/api/src/features/todos/dto/todo-response.dto.ts
@@ -0,0 +1,58 @@
+import { Expose, Type } from 'class-transformer'
+import { DefaultResponseDto } from 'src/common/dtos/default-response.dto'
+import { SlimCategoryResponseDto } from 'src/features/categories/dto/category-response.dto'
+import { ECategoryStages } from 'src/features/categories/enums/e-category-stages.enum'
+import { SlimIconResponseDto } from 'src/features/icons/dto/icon-response.dto'
+import { SlimUserResponseDto } from 'src/iam/users/dto/user-response.dto'
+
+export class TodoResponseDto extends DefaultResponseDto {
+  @Expose()
+  title: string
+
+  @Expose()
+  description?: string
+
+  @Expose()
+  progressStage?: string | ECategoryStages
+
+  @Expose()
+  startDuration?: Date
+
+  @Expose()
+  endDuration?: Date
+
+  @Expose()
+  @Type(() => SlimUserResponseDto)
+  userId: SlimUserResponseDto
+
+  @Expose()
+  @Type(() => SlimCategoryResponseDto)
+  categoryId?: SlimCategoryResponseDto
+
+  @Expose()
+  @Type(() => SlimIconResponseDto)
+  iconId?: SlimIconResponseDto
+
+  @Expose()
+  isEnabled: boolean
+}
+
+export class SlimTodoResponseDto {
+  @Expose()
+  id: string
+
+  @Expose()
+  title: string
+
+  @Expose()
+  progressStage?: string | ECategoryStages
+
+  @Expose()
+  startDuration?: Date
+
+  @Expose()
+  endDuration?: Date
+
+  @Expose()
+  isEnabled: boolean
+}
