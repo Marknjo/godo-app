833a9bc266820229de4602123d3470982d5a94aa
Mark Njoroge <marknjo7@gmail.com>
2023-11-08 13:08:47 +0300

    security(api/CategoryController): add base security rules to all methods

--- apps/api/src/features/categories/categories.controller.ts
+++ apps/api/src/features/categories/categories.controller.ts
@@ -14,8 +14,17 @@ import { PerseMongoIdPipe } from 'src/common/pipes/perse-mongo-id.pipe'
 import { ToggleCategoryStatusDto } from './dto/toggle-category-status.dto'
 import { Serialize } from 'src/common/decorators/serialize.decorator'
 import { CategoryResponseDto } from './dto/category-response.dto'
+import { Auth } from 'src/iam/authentication/decorators/auth.decorator'
+import { EAuthTypes } from 'src/iam/authentication/enums/e-auth-types.enum'
+import { AccessAuth } from 'src/iam/authorization/decorators/access-auth.decorator'
+import { EAccessAuthTypes } from 'src/iam/authorization/enums/e-access-auth-types.enum'
+import { RestrictToRole } from 'src/iam/authorization/decorators/restrict-to-role.decorator'
+import { eAllMembersMap, ePremiumSubscribers } from 'src/iam/enums/e-roles.enum'
 
 @Serialize(CategoryResponseDto)
+@RestrictToRole(...ePremiumSubscribers, ...eAllMembersMap)
+@AccessAuth(EAccessAuthTypes.ROLE)
+@Auth(EAuthTypes.BEARER)
 @Controller({
   path: 'categories',
   version: '1',
