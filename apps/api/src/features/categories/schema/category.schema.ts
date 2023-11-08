c66d7fdd7932ab7ccccec91ab19b92d8d36e3086
Mark Njoroge <marknjo7@gmail.com>
2023-11-08 12:24:52 +0300

    feat(api/categories): define Category schema

--- apps/api/src/features/categories/schema/category.schema.ts
+++ apps/api/src/features/categories/schema/category.schema.ts
@@ -1,1 +1,39 @@
-export class Category {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { HydratedDocument, SchemaTypes } from 'mongoose'
+import { ECategoryStages } from '../enums/e-category-stages.enum'
+import { Icon } from 'src/features/icons/schema/icon.schema'
+
+@Schema({
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+  timestamps: true,
+})
+export class Category {
+  @Prop({
+    index: 'text',
+  })
+  title: string
+
+  @Prop()
+  description?: string
+
+  @Prop({
+    index: true,
+  })
+  stages: Array<ECategoryStages | string>
+
+  @Prop({
+    type: SchemaTypes.ObjectId,
+    ref: 'Icon',
+  })
+  iconsId?: Icon
+
+  @Prop({
+    default: true,
+    index: true,
+  })
+  isEnabled: boolean
+}
+
+export const CategorySchema = SchemaFactory.createForClass(Category)
+export type TCategoryDoc = HydratedDocument<Category>
