84b74b94a3e9b18d16623941a7acbf5e5ccbab4c
Mark Njoroge <marknjo7@gmail.com>
2023-11-08 12:18:11 +0300

    feat(api): define CreateTodoDto validator

--- apps/api/src/features/todos/dto/create-todo.dto.ts
+++ apps/api/src/features/todos/dto/create-todo.dto.ts
@@ -1,1 +1,45 @@
-export class CreateTodoDto {}
+import { Type } from 'class-transformer'
+import {
+  IsDate,
+  IsMongoId,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator'
+import { ECategoryStages } from 'src/features/categories/enums/e-category-stages.enum'
+import { Category } from 'src/features/categories/schema/category.schema'
+import { Icon } from 'src/features/icons/schema/icon.schema'
+
+export class CreateTodoDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string
+
+  @IsOptional()
+  @MaxLength(500)
+  description?: string
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  progressStage?: string | ECategoryStages
+
+  @IsDate()
+  @IsOptional()
+  @Type(() => Date)
+  startDuration?: Date
+
+  @IsDate()
+  @IsOptional()
+  @Type(() => Date)
+  endDuration?: Date
+
+  @IsMongoId()
+  @IsOptional()
+  categoryId?: Category
+
+  @IsMongoId()
+  @IsOptional()
+  iconId?: Icon
+}
