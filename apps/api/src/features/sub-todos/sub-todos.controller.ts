b8efe14babd10ec4618f4f9b9f2adfe3676961fe
Mark Njoroge <marknjo7@gmail.com>
2023-11-08 13:09:15 +0300

    security(api/SubTodosController): add base security rules to all methods

--- apps/api/src/features/sub-todos/sub-todos.controller.ts
+++ apps/api/src/features/sub-todos/sub-todos.controller.ts
@@ -13,8 +13,17 @@ import { UpdateSubTodoDto } from './dto/update-sub-todo.dto'
 import { PerseMongoIdPipe } from 'src/common/pipes/perse-mongo-id.pipe'
 import { Serialize } from 'src/common/decorators/serialize.decorator'
 import { SubTodoResponseDto } from './dto/sub-todo-response.dto'
+import { AccessAuth } from 'src/iam/authorization/decorators/access-auth.decorator'
+import { Auth } from 'src/iam/authentication/decorators/auth.decorator'
+import { EAccessAuthTypes } from 'src/iam/authorization/enums/e-access-auth-types.enum'
+import { EAuthTypes } from 'src/iam/authentication/enums/e-auth-types.enum'
+import { RestrictToRole } from 'src/iam/authorization/decorators/restrict-to-role.decorator'
+import { eAllMembersMap, ePremiumSubscribers } from 'src/iam/enums/e-roles.enum'
 
 @Serialize(SubTodoResponseDto)
+@RestrictToRole(...ePremiumSubscribers, ...eAllMembersMap)
+@AccessAuth(EAccessAuthTypes.ROLE)
+@Auth(EAuthTypes.BEARER)
 @Controller({
   path: 'sub-todos',
   version: '1',
