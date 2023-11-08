bb952c137b7f461b1663fd19e14399f001217811
Mark Njoroge <marknjo7@gmail.com>
2023-11-08 12:26:14 +0300

    feat(api/categories): define CreateCategoryDto validator

--- apps/api/src/features/categories/dto/create-category.dto.ts
+++ apps/api/src/features/categories/dto/create-category.dto.ts
@@ -1,1 +1,35 @@
-export class CreateCategoryDto {}
+import { Icon } from 'src/features/icons/schema/icon.schema'
+import {
+  IsMongoId,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator'
+import { Transform } from 'class-transformer'
+import { ECategoryStages } from '../enums/e-category-stages.enum'
+
+export class CreateCategoryDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  @Transform(({ value }) => value.trim())
+  title: string
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(1000)
+  @Transform(({ value }) => value.trim())
+  description?: string
+
+  @IsOptional()
+  @IsNotEmpty({ each: true })
+  @IsString({ each: true })
+  @Transform(({ value }) => value.map((val: string) => val.trim()))
+  stages?: Array<ECategoryStages | string>
+
+  @IsOptional()
+  @IsMongoId()
+  iconsId?: Icon
+}
