8ea70b04f48fd39b2801d5a666a1c1e2b6c2fa6b
Mark Njoroge <marknjo7@gmail.com>
2023-11-09 11:07:47 +0300

    ci(api/IconsModule): import MongooseModule and setup IconsSchema

--- apps/api/src/features/icons/icons.module.ts
+++ apps/api/src/features/icons/icons.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common'
 import { IconsService } from './icons.service'
 import { IconsController } from './icons.controller'
+import { MongooseModule } from '@nestjs/mongoose'
+import { Icon, IconSchema } from './schema/icon.schema'
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: Icon.name,
+        useFactory: () => {
+          const schema = IconSchema
+
+          return schema
+        },
+      },
+    ]),
+  ],
   controllers: [IconsController],
   providers: [IconsService],
+  exports: [IconsService],
 })
 export class IconsModule {}
