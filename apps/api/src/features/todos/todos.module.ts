b2ac69421f5c74c5380c92ef28198768f704185e
Mark Njoroge <marknjo7@gmail.com>
2023-11-09 11:13:29 +0300

    ci(api/TodosModule): import MongooseModule and setup TodoSchema and other dependencies

--- apps/api/src/features/todos/todos.module.ts
+++ apps/api/src/features/todos/todos.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common'
 import { TodosService } from './todos.service'
 import { TodosController } from './todos.controller'
+import { MongooseModule } from '@nestjs/mongoose'
+import { Todo, TodoSchema } from './schema/todo.schema'
+import { CategoriesModule } from '../categories/categories.module'
+import { IconsModule } from '../icons/icons.module'
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: Todo.name,
+        useFactory: () => {
+          const schema = TodoSchema
+
+          return schema
+        },
+      },
+    ]),
+    CategoriesModule,
+    IconsModule,
+  ],
   controllers: [TodosController],
   providers: [TodosService],
 })
