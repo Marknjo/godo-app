42bdce74d03e3443b9013c4ff011c59e68390355
Mark Njoroge <marknjo7@gmail.com>
2023-11-09 11:08:13 +0300

    ci(api/SubTodosModule): import MongooseModule and setup SubTodoSchema

--- apps/api/src/features/sub-todos/sub-todos.module.ts
+++ apps/api/src/features/sub-todos/sub-todos.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common'
 import { SubTodosService } from './sub-todos.service'
 import { SubTodosController } from './sub-todos.controller'
+import { MongooseModule } from '@nestjs/mongoose'
+import { SubTodo, SubTodoSchema } from './schema/sub-todo.schema'
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: SubTodo.name,
+        useFactory: () => {
+          const schema = SubTodoSchema
+
+          return schema
+        },
+      },
+    ]),
+  ],
   controllers: [SubTodosController],
   providers: [SubTodosService],
+  exports: [SubTodosService],
 })
 export class SubTodosModule {}
