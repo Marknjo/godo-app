8293daee5f737bcd55a67438e045542468d155b0
Mark Njoroge <marknjo7@gmail.com>
2023-11-10 13:42:06 +0300

    feat(api/SubTodosService): implement throwIfNoIdAndFilters() helper

--- apps/api/src/features/sub-todos/sub-todos.service.ts
+++ apps/api/src/features/sub-todos/sub-todos.service.ts
@@ -61,4 +61,38 @@ export class SubTodosService {
   remove(subTodoId: string, activeUser: IActiveUser) {
     return `This action removes a #${subTodoId} subTodo`
   }
+
+  /**
+   * ---------------------------------------------
+   *
+   *                      HELPERS
+   *
+   * ---------------------------------------------
+   */
+
+  /**
+   * Ensures a users who does not provide a default sub-todo id
+   * also provides a filter option
+   *
+   * @param subTodoId
+   * @param filters
+   * @param whoIs
+   * @param action
+   */
+  private throwIfNoIdAndFilters(
+    subTodoId: string,
+    filters: {},
+    whoIs: string,
+    action: string,
+  ) {
+    if (!subTodoId && Object.values(filters).length === 0) {
+      this.logger.log(
+        `User with id ${whoIs} is trying ${action} without a sub-todo id and an alternative option other than user id`,
+      )
+
+      throw new BadRequestException(
+        `Failed to ${action} a todo: Please provide an alternative filter`,
+      )
+    }
+  }
 }
